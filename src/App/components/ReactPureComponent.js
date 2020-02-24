262c8be6474a2a53baf8bfe9967f09e6bf8efa1e
Ruslan Dzhafarov <Ruslan_Dzhafarov@epam.com>
2020-02-24 15:57:25 +0400

    feat: add React.PureComponent

---
+++ src/App/components/ReactPureComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class ReactPureComponent extends React.PureComponent {
+    render () {
+        return <div>Hello, this is PureReactComponent</div>
+    }
+}
+
+export default ReactPureComponent;
