52a02ec513185f9ad7b8feb7d59c2de5a62e844f
Ruslan Dzhafarov <Ruslan_Dzhafarov@epam.com>
2020-02-24 15:50:59 +0400

    fix: server logic

--- server.js
+++ server.js
@@ -7,12 +7,11 @@ const BUILD_DIR = path.join(__dirname, './build');
 const HTML_FILE = path.join(BUILD_DIR, 'index.html');
 
 app.use(express.static(BUILD_DIR));
-app.get('/api', (req, res) => {
-  res.send(mockResponse);
-});
+
 app.get('/', (req, res) => {
  res.sendFile(HTML_FILE);
 });
+
 app.listen(port, function () {
  console.log('App listening on port: ' + port);
 });
