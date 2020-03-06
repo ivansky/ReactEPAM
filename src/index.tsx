028a8e963f4b1a62069a56bc92b58c7c3b084740
Ruslan Dzhafarov <Ruslan_Dzhafarov@epam.com>
2020-03-06 15:47:51 +0400

    feat: add mocked data

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
-
+import {movies} from './mock-data';
+console.log(movies.data);
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/mock-data.ts
@@ -0,0 +1,133 @@
+export const movies =
+{
+    "data": [
+        {
+            "id": 284054,
+            "title": "Black Panther",
+            "tagline": "Long live the king",
+            "vote_average": 7.3,
+            "vote_count": 3788,
+            "release_date": "2018-02-13",
+            "poster_path": "https://image.tmdb.org/t/p/w500/uxzzxijgPIY7slzFvMotPv8wjKA.jpg",
+            "overview": "King T'Challa returns home from America to the reclusive, technologically advanced African nation of Wakanda to serve as his country's new leader. However, T'Challa soon finds that he is challenged for the throne by factions within his own country as well as without.  Using powers reserved to Wakandan kings, T'Challa assumes the Black Panther mantel to join with girlfriend Nakia, the queen-mother, his princess-kid sister,  members of the Dora Milaje (the Wakandan \"special forces\"), and an American secret agent, to prevent Wakanda from being dragged into a world war.",
+            "budget": 200000000,
+            "revenue": 1245257672,
+            "genres": [
+              "Action",
+              "Adventure",
+              "Fantasy",
+              "Science Fiction"
+            ],
+            "runtime": 134
+        },
+        {
+            "id": 333339,
+            "title": "Ready Player One",
+            "tagline": "A better reality awaits.",
+            "vote_average": 8.1,
+            "vote_count": 617,
+            "release_date": "2018-03-28",
+            "poster_path": "https://image.tmdb.org/t/p/w500/pU1ULUq8D3iRxl1fdX2lZIzdHuI.jpg",
+            "overview": "When the creator of a popular video game system dies, a virtual contest is created to compete for his fortune.",
+            "budget": 175000000,
+            "revenue": 0,
+            "genres": [
+              "Adventure",
+              "Science Fiction",
+              "Action"
+            ],
+            "runtime": 140
+        },
+        {
+            "id": 284053,
+            "title": "Thor: Ragnarok",
+            "tagline": "No Hammer. No Problem.",
+            "vote_average": 7.4,
+            "vote_count": 5349,
+            "release_date": "2017-10-25",
+            "poster_path": "https://image.tmdb.org/t/p/w500/rzRwTcFvttcN1ZpX2xv4j3tSdJu.jpg",
+            "overview": "Thor is imprisoned on the other side of the universe and finds himself in a race against time to get back to Asgard to stop Ragnarok, the prophecy of destruction to his homeworld and the end of Asgardian civilization, at the hands of an all-powerful new threat, the ruthless Hela.",
+            "budget": 180000000,
+            "revenue": 854229371,
+            "genres": [
+              "Action",
+              "Adventure",
+              "Fantasy"
+            ],
+            "runtime": 130
+        },
+        {
+            "id": 181808,
+            "title": "Star Wars: The Last Jedi",
+            "tagline": "The Saga Continues",
+            "vote_average": 7.1,
+            "vote_count": 4732,
+            "release_date": "2017-12-13",
+            "poster_path": "https://image.tmdb.org/t/p/w500/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+            "overview": "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+            "budget": 200000000,
+            "revenue": 1325937250,
+            "genres": [
+              "Fantasy",
+              "Adventure",
+              "Science Fiction"
+            ],
+            "runtime": 152
+        },
+        {
+            "id": 118340,
+            "title": "Guardians of the Galaxy",
+            "tagline": "All heroes start somewhere.",
+            "vote_average": 7.9,
+            "vote_count": 12376,
+            "release_date": "2014-07-30",
+            "poster_path": "https://image.tmdb.org/t/p/w500/y31QB9kn3XSudA15tV7UWQ9XLuW.jpg",
+            "overview": "Light years from Earth, 26 years after being abducted, Peter Quill finds himself the prime target of a manhunt after discovering an orb wanted by Ronan the Accuser.",
+            "budget": 170000000,
+            "revenue": 773328629,
+            "genres": [
+              "Action",
+              "Science Fiction",
+              "Adventure"
+            ],
+            "runtime": 121
+        },
+        {
+            "id": 228150,
+            "title": "Fury",
+            "tagline": "War never ends quietly.",
+            "vote_average": 7.4,
+            "vote_count": 4936,
+            "release_date": "2014-10-15",
+            "poster_path": "https://image.tmdb.org/t/p/w500/pfte7wdMobMF4CVHuOxyu6oqeeA.jpg",
+            "overview": "Last months of World War II in April 1945. As the Allies make their final push in the European Theater, a battle-hardened U.S. Army sergeant in the 2nd Armored Division named Wardaddy commands a Sherman tank called \"Fury\" and its five-man crew on a deadly mission behind enemy lines. Outnumbered and outgunned, Wardaddy and his men face overwhelming odds in their heroic attempts to strike at the heart of Nazi Germany.",
+            "budget": 68000000,
+            "revenue": 211817906,
+            "genres": [
+              "War",
+              "Drama",
+              "Action"
+            ],
+            "runtime": 135
+        },
+        {
+            "id": 300668,
+            "title": "Annihilation",
+            "tagline": "Fear What's Inside.",
+            "vote_average": 6.4,
+            "vote_count": 1291,
+            "release_date": "2018-02-22",
+            "poster_path": "https://image.tmdb.org/t/p/w500/d3qcpfNwbAMCNqWDHzPQsUYiUgS.jpg",
+            "overview": "A biologist signs up for a dangerous, secret expedition into a mysterious zone where the laws of nature don't apply.",
+            "budget": 40000000,
+            "revenue": 13258603,
+            "genres": [
+              "Mystery",
+              "Science Fiction",
+              "Drama"
+            ],
+            "runtime": 115
+        }
+    ],
+    "items": 7
+}
