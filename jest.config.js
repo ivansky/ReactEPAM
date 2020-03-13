386fd9af3a2ea3a00605cdcdc751c542ad929e75
Ruslan Dzhafarov <Ruslan_Dzhafarov@epam.com>
2020-03-13 12:12:21 +0400

    feat: add testing environment

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  'roots': [
+    '<rootDir>/src'
+  ],
+  'transform': {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  'testPathIgnorePatterns': ['\\\\node_modules\\\\'],
+  'transformIgnorePatterns': ['<rootDir>/node_modules/'],
+  'testRegex': '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
+  "setupFilesAfterEnv": [
+    "<rootDir>/jest.setup.js"
+  ],
+  'moduleFileExtensions': [
+    'ts',
+    'tsx',
+    'js',
+  ],
+  'globals': {
+    "ts-jest": {
+      'babelConfig': true,
+      'tsConfig': "tsconfig.json"
+    }
+  },
+  'collectCoverageFrom': ['src/**/*.{tsx}'],
+  'coverageDirectory': 'coverage',
+  "snapshotSerializers": ["enzyme-to-json/serializer"],
+}
