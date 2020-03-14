ea71fbeeebdc97b25ca1e9ca8cc5ac2d9b0f1ee4
Ruslan Dzhafarov <Ruslan_Dzhafarov@epam.com>
2020-03-14 19:48:54 +0400

    test: add snapshot test for App

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,5 +1,10 @@
-describe('My Test Suite', () => {
-    it('My Test Case', () => {
-      expect(true).toEqual(true);
-    });
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
+import App from './App';
+
+describe('<App/> rendering', () => {
+  const app = shallow(<App />)
+  it('should renders correctly',() => {
+      expect(app).toMatchSnapshot();
+  })
 });
